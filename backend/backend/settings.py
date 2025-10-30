1dca757b23c51b718727cdc69f30e35ef8706fc0
Ithran Beor Turno <turno.ithranbeor7@gmail.com>
2025-10-30 20:07:26 +0800

    s

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ DEBUG = False
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Set the static root for collectstatic
+# Set the static root for collectstati
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
